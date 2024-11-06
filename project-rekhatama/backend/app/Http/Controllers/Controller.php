a01f1be6ea044caec2fb246479e4cfcacb046c10
prkyakunutama0000 <prkyakunutama0000@gmail.com>
2024-11-06 12:35:33 +0700

    aweh bjir

--- project-rekhatama/backend/app/Http/Controllers/Controller.php
+++ project-rekhatama/backend/app/Http/Controllers/Controller.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use GuzzleHttp\Psr7\Request;
 
+// naha aweh anjir
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
